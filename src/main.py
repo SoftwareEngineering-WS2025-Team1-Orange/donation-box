83c034340048d2997615d94d5650a5f28ec03582
Lukas Limmer <lukas.limmer@tum.de>
2024-11-28 15:20:06 +0100

    disable API endpoints

--- src/main.py
+++ src/main.py
@@ -8,18 +8,18 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/pull")
+#@app.get("/pull")
 def pull_container():
     subprocess.run("docker pull nginx", shell=True)
     return {"Hello": "World"}
 
 
-@app.get("/run")
+#@app.get("/run")
 def run_container():
     subprocess.run("docker run --name nginx nginx", shell=True)
     return {"Hello": "World"}
 
-@app.get("/stop")
+#@app.get("/stop")
 def start_container():
     subprocess.run("docker stop nginx", shell=True)
     subprocess.run("docker rm nginx", shell=True)
