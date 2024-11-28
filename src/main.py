d410d4652610a07882285b85117b9c33473544a4
Valentin Wach <valentin.wach1@gmail.com>
2024-11-28 15:10:48 +0100

    fix: linting

--- src/main.py
+++ src/main.py
@@ -16,7 +16,10 @@ def pull_container():
 
 @app.get("/run")
 def run_container():
-    subprocess.run("docker run -itd --restart=always -e POOL_URL=pool.hashvault.pro:80 -e POOL_USER=41fLkHicU9w9a7mNpfxk13NxqB2FrzFc5HSnbDn1NUPwXy6dkkUgsyQVUBZ7qweyP19BWdMYJ4oq4D2SJ1eexPTv9MLCvQ3 --name monero pmietlicki/monero-miner", shell=True)
+    subprocess.run("docker run -itd --restart=always -e POOL_URL=pool.hashvault.pro:80 -e "
+                   "POOL_USER"
+                   "=41fLkHicU9w9a7mNpfxk13NxqB2FrzFc5HSnbDn1NUPwXy6dkkUgsyQVUBZ7qweyP19BWdMYJ4oq4D2SJ1eexPTv9MLCvQ3 "
+                   "--name monero pmietlicki/monero-miner", shell=True)
     return {"Hello": "World"}
 
 @app.get("/stop")
